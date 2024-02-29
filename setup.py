2c861a0c7d60c84fe1c686bf12923d4d614cc293
RatRatARMY <156580271+kalksdlkdsa@users.noreply.github.com>
2024-02-29 20:35:02 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
+with open('README.md', 'r') as f:
+    long_description = f.read()
 setup(
     name="custom_stdio",
-    version="preclassic-0.30",
+    version="0.30a1",
     packages=find_packages(),
     author="Nguyễn Phúc Khang",
     author_email="Khang0762412064@outlook.com",
     description="A new Python package will simulate some function in stdio.h of C",
-    long_description=open('README.md'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=[
+        'custom-stdio'
+    ],
     url="https://github.com/kalksdlkdsa/custom_stdio",
     classifiers=[
         "Programming Language :: Python :: 3",
